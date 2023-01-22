02e3f9186116ad19d224f269ffde5c0f78272bdd
Wayne咏辰 <75261164+wayne931121@users.noreply.github.com>
2023-01-22 21:18:05 +0800

    Update ffmpeg.py

--- ffmpeg.py
+++ ffmpeg.py
@@ -1,18 +1,41 @@
-input = "input.mp4"
-output = "output.mp4"
-string = "0:0 1:0,speed=3 2:0 3:0"
+import argparse
+parser = argparse.ArgumentParser(description="Help write triming video code in ffmpeg command by giving text file.")
+parser.add_argument("-file", "-f", help="input text file, including triming time.", type=str, default="trim.txt")
+parser.add_argument("-text", "-t", help="input text, including triming time.", type=str, default="")
+parser.add_argument("-input", "-i", help="input video file.", type=str, default="input.mp4")
+parser.add_argument("-output", help="output video file.", type=str, default="output.mp4")
+parser.add_argument("-mute", "-m", help="mute video.", action="store_true")
+args = parser.parse_args()
+
+#input = "input.mp4"
+#output = "output.mp4"
+#string = "0:0 1:0,speed=3 2:0 3:0"
+input, string, output = args.input, args.text, args.output
+if not string:
+  try:
+    f = open(args.file, "r", encoding="utf-8")
+    string = f.read()
+    f.close()
+  except Exception as e:
+    print(e)
+    print("Use Default Settting.\n")
+    string = "0:0 1:0,speed=3 2:0 3:0\n"
 string = string.replace("\n", " ")
 string = string.replace(", ", ",").replace(",  ", ",").replace(",   ", ",")
 string = string.replace("= ", "=").replace(" = ", "=").replace(" =", "=")
 while ("  " in string):
   string = string.replace("  ", " ")
 arrays = string.split(" ")
+for check_empty in range(0, len(arrays))[::-1]:
+  if not arrays[check_empty]: arrays.pop(check_empty)
+print("Use setting:", arrays, "\n")
 i = 0
 iv = 0
 ia = 0
 video = ""
 audio = ""
-all_mute = False
+#all_mute = False
+all_mute = args.mute
 while i<len(arrays)-1:
   filters = arrays[i].split(",")
   startTime = filters[0].replace(":", "\\\\:")
